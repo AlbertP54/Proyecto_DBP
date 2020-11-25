54570ab6e75ca5fe83b25b4f067559ea8269b681
Alberto Payan <a311047@uach.mx>
2020-11-24 18:04:55 -0800

    Creando archivos

---
+++ Server/app.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const log4js = require('log4js');
+
+const logger = log4js.getLogger();
+logger.level = 'debug';
+
+http.createServer((request, response) =>{
+
+    if(request.url) {
+        const file = request.url == '/' ? './WWW/index.html' : `./WWW${request.url}`;
+        fs.readFile(file, (err, data) =>{
+            if(err){
+                response.writeHead(404, {"Content-Type": "text/html"});
+                logger.warn(`NOT FOUND ${request.method} ${request.url}`)
+                response.write("NOT FOUND");
+            } else {
+                logger.info(`${request.method} ${request.url}`);
+               
+                if(file.split('.').pop() == 'jpg') 
+                    response.writeHead(200, {"Content-Type": "image/jpg"});
+                else if(file.split('.').pop() == 'css')
+                    response.writeHead(200, {"Content-Type": "text/css"});
+                else
+                    response.writeHead(200, {"Content-Type": "text/html"});
+                // Contenido del cuerpo de la respuesta
+                response.write(data);
+            }
+            response.end();
+        });
+    }
+}).listen(4000);
