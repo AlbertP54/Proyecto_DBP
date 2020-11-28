a9595e4237c1bfcb52fc390a39a71ae175d727a5
Ericka Bermúdez <erickabdz@gmail.com>
2020-11-28 13:53:29 -0700

    added response for .json

--- server/app.js
+++ server/app.js
@@ -1,31 +1,40 @@
 const http = require('http');
 const fs = require('fs');
 const log4js = require('log4js');
+const about = require('./WWW/about.json');
 
 const logger = log4js.getLogger();
 logger.level = 'debug';
 
-http.createServer((request, response) =>{
+http.createServer((request, response) => {
 
-    if(request.url) {
-        const file = request.url == '/' ? './www/index.html' : `./www${request.url}`;
-        fs.readFile(file, (err, data) =>{
-            if(err){
-                response.writeHead(404, {"Content-Type": "text/html"});
-                logger.warn(`NOT FOUND ${request.method} ${request.url}`)
-                response.write("NOT FOUND");
-            } else {
-                logger.info(`${request.method} ${request.url}`);
-               
-                if(file.split('.').pop() == 'jpg') 
-                    response.writeHead(200, {"Content-Type": "image/jpg"});
-                else if(file.split('.').pop() == 'css')
-                    response.writeHead(200, {"Content-Type": "text/css"});
-                else
-                    response.writeHead(200, {"Content-Type": "text/html"});
-                response.write(data);
-            }
-            response.end();
-        });
+    if (request.url) {
+
+        // .json response for the team members
+        if (request.url == '/about') {
+            response.writeHead(200, { "Content-Type": "application/json" });
+            response.write(JSON.stringify(about));
+        } else {
+            const file = request.url == '/' ? './WWW/index.html' : `./WWW${request.url}`;
+            fs.readFile(file, (err, data) => {
+                if (err) {
+                    response.writeHead(404, { "Content-Type": "text/html" });
+                    logger.warn(`NOT FOUND ${request.method} ${request.url}`)
+                    response.write("NOT FOUND");
+                } else {
+                    logger.info(`${request.method} ${request.url}`);
+
+                    if (file.split('.').pop() == 'jpg')
+                        response.writeHead(200, { "Content-Type": "image/jpg" });
+                    else if (file.split('.').pop() == 'css')
+                        response.writeHead(200, { "Content-Type": "text/css" });
+                    else
+                        response.writeHead(200, { "Content-Type": "text/html" });
+                    response.write(data);
+                }
+                response.end();
+            });
+        }
+
     }
 }).listen(4000);
