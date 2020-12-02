97da443fbb4eec3c58bbe71706c1b8ea0319e8c8
Alberto Payan <a311047@uach.mx>
2020-12-02 00:01:47 -0800

    Aded cv and contact form

--- server/www/scripts.js
+++ server/www/scripts.js
@@ -45,3 +45,46 @@ function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function validation(){
+    var name = document.getElementById("name").value;
+    var subject = document.getElementById("subject").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
+    var text;
+
+    if(name.length < 5){
+        text = "Please Enter Valid Name";
+        error_message.innerHTML = text;
+        return false;
+    }
+
+    if(subject.length < 10){
+        text = "Please Enter Correct Subject";
+        error_message.innerHTML = text;
+        return false;
+    }
+
+    if(isNaN(phone) || phone.length != 10){
+        text = "Please Enter Valid Phone Number";
+        error_message.innerHTML = text;
+        return false;
+    }
+
+    if(email.indexOf("@") == -1 || email.length < 6){
+        text = "Please Enter Valid Email";
+        error_message.innerHTML = text;
+        return false;
+    }
+
+    if(message.lenght <= 40){
+        text = "Please Enter More Than 40 Characters";
+        error_message.innerHTML = text;
+        return false;
+    }
+
+    alert("Form Submitted Succesfully!");
+    return true;
+}
+
