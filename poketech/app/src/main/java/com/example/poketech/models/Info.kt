ef605ed58ccabc2d83bafbb001eb530de4b2493f
Ericka Bermúdez <erickabdz@gmail.com>
2020-12-02 01:30:59 -0700

    fixed thing with app
    i... saved the folder in the wrong place... sorry....

--- poketech/app/src/main/java/com/example/poketech/models/Info.kt
+++
@@ -1,5 +0,0 @@
-package com.example.poketech.models
-
-class Info(val servidor:String, val equipo:Array<Trainer>){
-
-}
--- poketech/app/src/main/java/com/example/poketech/models/Trainer.kt
+++
@@ -1,5 +0,0 @@
-package com.example.poketech.models
-
-class Trainer(val name: String, val lastName: String){
-
-}
